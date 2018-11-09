1b09cfc731d340695f3ecb26a502547109407fee
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-11-09 10:56:52 -0300

    Create feature analyzer

---
+++ code/generate_analysis.py
@@ -0,0 +1,15 @@
+###############################################################################
+
+from io_helper import IOHelper
+from statistics_helper import StatisticsHelper
+
+
+
+if __name__ == "__main__":
+
+	train_data = IOHelper.read_dataset("train")
+	for col in train_data.columns:
+		series= train_data[col]
+		stats = StatisticsHelper.getFeatureStats(series)
+		figure = StatisticsHelper.drawFeatureDistribution(series, col)
+		IOHelper.store_analysis(stats, col)
