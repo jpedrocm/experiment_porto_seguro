323de5f0b205186b5a7ce0c2eb5d2b64f0a29864
João Pedro Magalhães <5193458+jpedrocm@users.noreply.github.com>
2018-11-10 19:06:31 -0300

    add more stats to helper

--- code/statistics_helper.py
+++ code/statistics_helper.py
@@ -11,10 +11,12 @@ class StatisticsHelper():
 		stats = series.describe()
 		stats["nan_count"] = len(series)-series.count()
 		stats["nb_unique_values"] = series.nunique()
+		stats["col_nan_rate"] = stats["nan_count"]/stats["count"]
+		stats["std_fraction_of_mean"] = stats["std"]/stats["mean"]
+		stats["dtype"] = series.dtype
 		return DataFrame(stats)
 
 	@staticmethod
 	def drawFeatureDistribution(series, col):
 		min_bins = min(20, series.nunique())
-		return series.plot.hist(bins=min_bins, title=col, grid=True,
-			                    rwidth=0.99)
+		return series.plot.hist(bins=min_bins, title=col, grid=True)
